82f436d5ff102ec7177eb888de28f0cb30a68f4a
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-01-30 21:43:27 -0300

    realiza cadastros

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,58 @@ expansor.addEventListener('click', (event) => {
   document.getElementById('expansor').classList.add('mostrar');
  
   });
+const conta = [];
+const enviar = document.getElementById('enviar');
+const form = document.getElementById('form');
+
+
+
+enviar.addEventListener('click', (event)=>{
+event.preventDefault();
+const nome = form.nome.value.trim();
+const celular = form.celular.value.trim();
+const cpf = form.cpf.value.trim();
+const confirmed_senha = form.confirmed_senha.value.trim();
+const senha = form.senha.value.trim();
+ 
+if(!nome){alert('nome vazio');}
+else if(cpf.length != 14){alert('cpf incorreto');}
+else if(celular.length != 11){alert('celular incorreto');}
+else if(!senha){alert('senha vazia');}
+else if(confirmed_senha != senha){alert('senhas diferentes');}
+else{
+  conta.push(
+    { 'nome': nome, 'cpf': cpf, 'celular': celular, 'senha': senha, conta: new Date().getTime(), saldo: 0, }
+  );
+ alert('Conta criada com sucesso!');
+}
+
+
+}
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
